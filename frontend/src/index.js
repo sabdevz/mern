d307ea9eaa8b1a399f626b7c85306e1be7d22310
rgsabareesh <sabareesh.rg@perfomatix.com>
2022-03-28 23:03:54 +0530

    base react app added for frontend

---
+++ frontend/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
